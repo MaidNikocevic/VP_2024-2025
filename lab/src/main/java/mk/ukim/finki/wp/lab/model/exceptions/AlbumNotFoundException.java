ce1d21bfc47ce94e938545b79f029eb9c3cda4d5
MaidNikocevic <164071606+MaidNikocevic@users.noreply.github.com>
2024-11-27 18:44:58 +0100

    Delete lab directory

--- lab/src/main/java/mk/ukim/finki/wp/lab/model/exceptions/AlbumNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.wp.lab.model.exceptions;
-
-public class AlbumNotFoundException extends RuntimeException {
-    public AlbumNotFoundException(Long id) {
-        super(String.format("Album with id %d was not found.", id));
-    }
-}
--- lab/src/main/java/mk/ukim/finki/wp/lab/model/exceptions/SongNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.wp.lab.model.exceptions;
-
-public class SongNotFoundException extends RuntimeException {
-    public SongNotFoundException(Long id) {
-        super(String.format("Song with id %d was not found.", id));
-    }
-}
